bbef5dcd9bdf1863d3c1b0f38936d031602cbb38
BenjaminWyss <b040w377@ku.edu>
2019-02-28 13:55:05 -0600

    Fix bug where question 3 was not being registered properly.

--- quiz.php
+++ quiz.php
@@ -1,7 +1,7 @@
 <?php
   $q1 = $_POST["9+10"];
   $q2 = $_POST["7*8"];
-  $q3 = $_POST["3/5 of 60"];
+  $q3 = $_POST["3/5of60"];
   $q4 = $_POST["derivative"];
   $q5 = $_POST["trains"];
 
@@ -13,8 +13,29 @@
     $correct++;
     $score += 20;
   }
+  if ($q2 == "56")
+  {
+    $correct++;
+    $score += 20;
+  }
+  if ($q3 == "36")
+  {
+    $correct++;
+    $score += 20;
+  }
+  if ($q4 == "72x")
+  {
+    $correct++;
+    $score += 20;
+  }
+  if ($q5 == "2.5 Hours")
+  {
+    $correct++;
+    $score += 20;
+  }
 
 
   echo "<h1>Quiz Results</h1><hr>";
   echo "<p>Total Correct Answers: " . $correct . " / 5</p>";
+  echo "<p>Total Score: " . $score . "%</p>";
 ?>
