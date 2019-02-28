ab2e5401cc10f64ed80cd97da039d5125c6be9e8
BenjaminWyss <b040w377@ku.edu>
2019-02-28 13:25:26 -0600

    fix table formatting on multiplicationTable.php

--- multiplicationTable.php
+++ multiplicationTable.php
@@ -1,17 +1,17 @@
 <?php
-  echo "<h>1-100 Multiplication Table</h><hr>";
-  echo "<table><tr><th>#</th>";
+  echo "<h1>1-100 Multiplication Table</h1><hr>";
+  echo "<table><tr><td>#</td>";
   for($i = 1; $i <= 100; $i++)
   {
-    echo "<th>" . $i . "</th>";
+    echo "<td>" . $i . "</td>";
   }
   for($i = 1; $i <= 100; $i++)
   {
     echo "</tr><tr>";
-    echo "<th>" . $i . "</th>";
+    echo "<td>" . $i . "</td>";
     for($j = 1; $j <= 100; $j++)
     {
-
+      echo "<td>" . $i * $j . "</td>";
     }
   }
   echo "</tr></table>";
