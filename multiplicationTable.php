0a3f560a2fc82959209dafa6fffc2551c2949d4b
BenjaminWyss <b040w377@ku.edu>
2019-02-28 13:02:50 -0600

    Create framework for multiplicationTable.php

---
+++ multiplicationTable.php
@@ -0,0 +1,11 @@
+<?php
+  echo "<h>1-100 Multiplication Table</h>";
+  echo "<table><tr>";
+  for($i = 0; $i <= 100; $i++)
+  {
+    echo "<th>" . $i . "</th>";
+  }
+  echo "</tr>";
+
+  echo "</table>";
+ ?>
