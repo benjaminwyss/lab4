4afc65d08f43168492d23d055efb25a11246bde6
BenjaminWyss <b040w377@ku.edu>
2019-03-01 10:33:41 -0600

    Finish validateForm Function.

--- formChecker.js
+++ formChecker.js
@@ -23,4 +23,61 @@ function validateForm()
     return false
   }
 
+  if (isNaN(parseInt(animalFarms)))
+  {
+    alert("Please enter an integer quantity of Animal Farm Books (0 is Okay).")
+    return false
+  }
+  if (parseInt(animalFarms) < 0)
+  {
+    alert("Please enter a non-negative quantity of Animal Farm Books (0 is Okay).")
+    return false
+  }
+  if (parseInt(animalFarms) != animalFarms)
+  {
+    alert("Please enter a whole number quantity of Animal Farm Books (0 is Okay).")
+    return false
+  }
+
+  if (isNaN(parseInt(catchers)))
+  {
+    alert("Please enter an integer quantity of The Catcher in the Rye Books (0 is Okay).")
+    return false
+  }
+  if (parseInt(catchers) < 0)
+  {
+    alert("Please enter a non-negative quantity of The Catcher in the Rye Books (0 is Okay).")
+    return false
+  }
+  if (parseInt(catchers) != catchers)
+  {
+    alert("Please enter a whole number quantity of The Catcher in the Rye Books (0 is Okay).")
+    return false
+  }
+
+  if (isNaN(parseInt(enders)))
+  {
+    alert("Please enter an integer quantity of Ender's Game Books (0 is Okay).")
+    return false
+  }
+  if (parseInt(enders) < 0)
+  {
+    alert("Please enter a non-negative quantity of Ender's Game Books (0 is Okay).")
+    return false
+  }
+  if (parseInt(enders) != enders)
+  {
+    alert("Please enter a whole number quantity of Ender's Game Books (0 is Okay).")
+    return false
+  }
+
+  if (shipping == "")
+  {
+    alert("Please select a shipping option.")
+    return false
+  }
+
+  //all checks validated
+  return true
+
 }
