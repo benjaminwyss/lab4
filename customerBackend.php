0e7fa4fbc0cb7e76b938e1f9960c8ba030383286
BenjaminWyss <b040w377@ku.edu>
2019-03-01 10:34:09 -0600

    Add variables to php backend page

---
+++ customerBackend.php
@@ -0,0 +1,8 @@
+<?php
+$username = $_POST["username"];
+$password = $_POST["password"];
+$animalFarms = $_POST["animalFarm"];
+$catchers = $_POST["catcherInTheRye"];
+$enders = $_POST["endersGame"];
+$shipping = $_Post["shipping"];
+?>
