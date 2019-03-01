c9060d25882c9b0039f365bd3e479ebaeccf35b2
BenjaminWyss <b040w377@ku.edu>
2019-03-01 10:51:46 -0600

    Finish framework for backend.

--- customerBackend.php
+++ customerBackend.php
@@ -9,6 +9,18 @@
   $enders = $_POST["endersGame"];
   $shipping = $_Post["shipping"];
 
-  echo "<h4>Welcome User " . $username . "</h4>";
+  echo "<h4>Welcome and thank you for your purchase " . $username . "!</h4>";
+  echo "<p>Your password is: " . $password . "</p><p>Remember not to share your password with anyone!</p><hr>";
+  echo "<h4>Your Receipt:</h4>";
 
+  echo "<table><tr>";
+  echo "<th>Purchase</th><th>Quantity</th><th>Cost Per Item</th><th>Subtotal</th></tr>";
+
+  echo "<tr><th>Animal Farm</th></tr>";
+
+  echo "<tr><th>The Catcher in the Rye</th></tr>";
+
+  echo "<tr><th>Ender's Game</th></tr>";
+
+  echo "</table>";
 ?>
