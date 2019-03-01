f21b5d07e12cd637523d1aba826f5671e7133fcd
BenjaminWyss <b040w377@ku.edu>
2019-03-01 10:41:49 -0600

    Add css styling to php page

--- customerBackend.php
+++ customerBackend.php
@@ -1,8 +1,14 @@
 <?php
-$username = $_POST["username"];
-$password = $_POST["password"];
-$animalFarms = $_POST["animalFarm"];
-$catchers = $_POST["catcherInTheRye"];
-$enders = $_POST["endersGame"];
-$shipping = $_Post["shipping"];
+  $css = file_get_contents("style.css");
+  echo "<style>" . $css . "</style>";
+
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  $animalFarms = $_POST["animalFarm"];
+  $catchers = $_POST["catcherInTheRye"];
+  $enders = $_POST["endersGame"];
+  $shipping = $_Post["shipping"];
+
+  echo "<h4>Welcome User " . $username . "</h4>";
+
 ?>
